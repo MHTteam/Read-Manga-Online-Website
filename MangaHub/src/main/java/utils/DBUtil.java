9641f577736ba4071d0753f6a81a714ef05027f9
Dao Minh Tri <daominhtri1000@gmail.com>
2022-08-22 14:36:48 +0700

    add controller

--- MangaHub/src/main/java/utils/DBUtil.java
+++ MangaHub/src/main/java/utils/DBUtil.java
@@ -5,10 +5,36 @@
  */
 package utils;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 /**
  *
  * @author tri
  */
 public class DBUtil {
-    
+
+    public static Connection getConnection() {
+        try {
+            String url = "jdbc:sqlserver://" + serverName + ":" + portNumber + "\\" + instance + ";databaseName=" + dbName;
+            if (instance == null || instance.trim().isEmpty()) {
+                url = "jdbc:sqlserver://" + serverName + ":" + portNumber + ";databaseName=" + dbName;
+            }
+            Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+            return DriverManager.getConnection(url, userID, password);
+        } catch (SQLException ex) {
+            System.out.println("Connection error! " + ex.getMessage());
+        } catch (ClassNotFoundException ex) {
+            System.out.println("Connection error! " + ex.getMessage());
+        }
+        return null;
+    }
+    private final static String serverName = "localhost";
+    private final static String dbName = "MANGAHUB";
+    private final static String portNumber = "1433";
+    private final static String instance = "";//LEAVE THIS ONE EMPTY IF YOUR SQL IS A SINGLE INSTANCE
+    private final static String userID = "sa";
+    private final static String password = "123456";
+
 }
